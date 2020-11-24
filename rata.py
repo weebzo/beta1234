f92509aecb26d6dda1e66322487c79b7a3b7ccbb
zmql <74221482+zmql@users.noreply.github.com>
2020-11-24 13:32:54 -0500

    Update rata.py

--- rata.py
+++ rata.py
@@ -20,18 +20,7 @@ bytes = random._urandom(1490)
 os.system("clear")
 os.system("figlet DDos Attack")
 print
-print "
- ██▀███   ▄▄▄     ▄▄▄█████▓ ▄▄▄      
-▓██ ▒ ██▒▒████▄   ▓  ██▒ ▓▒▒████▄    
-▓██ ░▄█ ▒▒██  ▀█▄ ▒ ▓██░ ▒░▒██  ▀█▄  
-▒██▀▀█▄  ░██▄▄▄▄██░ ▓██▓ ░ ░██▄▄▄▄██ 
-░██▓ ▒██▒ ▓█   ▓██▒ ▒██▒ ░  ▓█   ▓██▒
-░ ▒▓ ░▒▓░ ▒▒   ▓▒█░ ▒ ░░    ▒▒   ▓▒█░
-  ░▒ ░ ▒░  ▒   ▒▒ ░   ░      ▒   ▒▒ ░
-  ░░   ░   ░   ▒    ░        ░   ▒   
-   ░           ░  ░              ░  ░
-                                     
-"
+print "This a beta so dont send attackz to anyone"
 print
 ip = raw_input("IP Target : ")
 port = input("Port       : ")
